57dcc08d9c16634a814a6f49b7d20154a29172b2
chaojie <zhuzhezhe95@gmail.com>
2022-01-09 19:03:09 +0800

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by 朱哲哲 on 2022/1/9.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
